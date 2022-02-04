7fc2c71f71e1901d3fe85600f384327b069b0c90
nrcool <nomanalam7214@gmail.com>
2022-02-05 00:47:46 +0100

    fix bug in roleMiddleware

--- middlewares/RoleMiddleware.js
+++ middlewares/RoleMiddleware.js
@@ -1,7 +1,7 @@
 exports.roleCheck=(req,res,next)=>{
     if(req.user.role==="admin"){
         next()
-    }else if(req.params.id===req.user._id || user.blogs.includes(req.params.id)){
+    }else if(req.params.id===req.user._id.toString() || user.blogs.includes(req.params.id)){
         next()
     }else{
         let err= new Error("you don't have right permissions to perform such action")
